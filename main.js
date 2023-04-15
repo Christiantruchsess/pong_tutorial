fa5526c0ce79f179f28fdd5fb845045238191b78
Christian Truchsess <63025023+Christiantruchsess@users.noreply.github.com>
2023-04-15 13:49:15 -0400

    added this.radius attribute to Ball Class

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 class Ball {
-    constructor(startX, startY, startColor) {
+    constructor(startX, startY, startRadius, startColor) {
         this.xPosition = startX
         this.yPosition = startY
+        this.radius = startRadius
         this.color = startColor
         this.xDirection = 1
         this.yDirection = 1
@@ -10,7 +11,7 @@ class Ball {
     draw (ctx) {
         ctx.fillStyle = this.color
         ctx.beginPath()
-        ctx.arc(this.xPosition,this.yPosition,15,0,2*Math.PI)
+        ctx.arc(this.xPosition,this.yPosition,this.radius,0,2*Math.PI)
         ctx.fill() 
     }
 
@@ -38,9 +39,9 @@ class Ball {
 // Define Global Variables 
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
-var ball1 = new Ball(300, 300, "blue")
-var ball2 = new Ball(200,200, "green")
-var ball3 = new Ball(100,100,'purple')
+var ball1 = new Ball(300,300,15,"blue")
+var ball2 = new Ball(200,200,15,"green")
+var ball3 = new Ball(100,100,15,"purple")
 
 // Define Functions
 
