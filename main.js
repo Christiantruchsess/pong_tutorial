2dcb1f4ab4b202a3b196250bd11fdbfef401b5c2
Christian Truchsess <63025023+Christiantruchsess@users.noreply.github.com>
2023-04-23 14:59:20 -0400

    simplified update and draw functions

--- main.js
+++ main.js
@@ -51,22 +51,59 @@ var ball1 = new Ball(300,300,15,"blue")
 var ball2 = new Ball(200,200,15,"green")
 var ball3 = new Ball(100,100,15,"purple")
 
+var mousePosition = {
+    x: 0,
+    y: 0
+}
+
+var entities = []
+entities.push(ball1)
+entities.push(ball2)
+entities.push(ball3)
+
 // Define Functions
 
 function draw() {
     // draws "Canvas"
     ctx.clearRect(0,0,canvas.clientWidth,canvas.clientHeight)
     // draws ball instances
-    ball1.draw(ctx)
-    ball2.draw(ctx)
-    ball3.draw(ctx)
+    // ball1.draw(ctx)
+    // ball2.draw(ctx)
+    // ball3.draw(ctx)
+
+    entities.forEach(eachEntityDraw)
+
+    // for(var index = 0; index < entities.length; index += 1) {
+    //     eachEntityDraw(entities[index])
+    // }
+
+    // var index = 0
+    // while(index < entities.length) {
+    //     eachEntityDraw(entities[index])
+    //     index += 1
+    // }
+}
+
+
+
+
+function eachEntityDraw(entity) {
+    entity.draw(ctx)
+}
+
+function eachEntityUpdate(entity) {
+    entity.update()
 }
 
 function update() {
     // calls ball's "update" function; moves ball by updating ball's position
-    ball1.update()
-    ball2.update()
-    ball3.update()
+    //ball1.update()
+    //ball2.update()
+    //ball3.update()
+    // ball3.xPosition = mousePosition.x
+    // ball3.yPosition = mousePosition.y
+    entities.forEach(eachEntityUpdate)
+
 }
 
 function animate() {
@@ -77,10 +114,33 @@ function animate() {
 }
 
 function onContentLoaded() {
+
     // defines "canvas" and its "context"; initializes program by calling "animate" function
     canvas = document.getElementById("canvas");
+
+    canvas.addEventListener("click", onClick)
+
+    canvas.addEventListener("mousemove", onMousemove)
+
+    window.addEventListener("keydown", onKeyDown)
+
+
     ctx = canvas.getContext("2d");
     animate()
+}
+
+function onClick(event) {
+    console.log("clicked!", event)
+}
+
+function onMousemove(event) {
+    //console.log("mouse moved!", event)
+    mousePosition.x = event.clientX
+    mousePosition.y = event.clientY
+}
+
+function onKeyDown(event) {
+    console.log("key down!", event)
 }
 
 // "turns on the ignition" by waiting for HTML Content to load, at which point "onContentLoaded" function is run
